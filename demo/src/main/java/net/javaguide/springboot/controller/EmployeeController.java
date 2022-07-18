0fbc693b4497d10544019c6a5a9177f2228bb95a
yingnamlee <yingnamlee@localhost>
2022-07-18 18:51:38 -0400

    First Commit

---
+++ demo/src/main/java/net/javaguide/springboot/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package net.javaguide.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguide.springboot.model.Employee;
+import net.javaguide.springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployee(){
+		return employeeRepository.findAll();
+	}
+}
---
+++ demo/src/main/java/net/javaguide/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package net.javaguide.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguide.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+
+}
